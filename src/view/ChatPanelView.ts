ac1e470362e09572d599d09423cf12ba3ab3059e
Marco <mirco.masera@gmail.com>
2025-04-01 18:06:55 +0200

    Dev: chat panel

--- src/view/ChatPanelView.ts
+++ src/view/ChatPanelView.ts
@@ -47,29 +47,46 @@ export class ChatPanelView implements vscode.WebviewViewProvider {
 		const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'vscode.css'));
         const style = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'chat_panel_style.css'));
         const codiconsUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'node_modules', '@vscode/codicons', 'dist', 'codicon.css'));
-		// Use a nonce to only allow a specific script to be run.
+		const snakemakerIcon = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'resources', 'icon.png'))
+        // Use a nonce to only allow a specific script to be run.
 		const nonce = getNonce();
 
-		return `<!DOCTYPE html>
-			<html lang="en">
-			<head>
-				<meta charset="UTF-8">
-				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; font-src ${webview.cspSource}; style-src ${webview.cspSource};">
+        return `<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${webview.cspSource}; font-src ${webview.cspSource}; style-src ${webview.cspSource};">
 
-				<meta name="viewport" content="width=device-width, initial-scale=1.0">
-				<link href="${styleVSCodeUri}" rel="stylesheet">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link href="${styleVSCodeUri}" rel="stylesheet">
                 <link href="${style}" rel="stylesheet">
                 <link href="${codiconsUri}" rel="stylesheet">
-				<title>Cat Colors</title>
-			</head>
-			<body>
+                <title>Cat Colors</title>
+            </head>
+            <body>
+
+                <!-- Example user message -->
+                <div class="chat-user-container">
+                    <p><strong>User</strong></p>
+                    <p>Hello, how are you?</p>
+                </div>
+
+                <!-- Example bot message -->
+                <div class="chat-bot-container">
+                    <div class="bot-header">
+                        <img src="${snakemakerIcon}" class="bot-icon"></img>
+                        <strong>Snakemaker</strong>
+                    </div>
+                    <p>I'm fine, thank you!</p>
+                </div>
+
                 <div id="chat-textarea-container">
-				    <textarea id="input" rows="10" cols="30"></textarea>
+                    <textarea id="input" rows="10" cols="30"></textarea>
                     <div id="send-button" class="codicon codicon-send"></div>
                 </div>
-				<script nonce="${nonce}" src="${scriptUri}"></script>
-			</body>
-			</html>`;
+                <script nonce="${nonce}" src="${scriptUri}"></script>
+            </body>
+            </html>`;
 	}
 }
 
